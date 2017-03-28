b13d999ac108cc50d510c398861dd5c3d227ee0d
Meriem Chabbouh <meriemchabbouh1@gmail.com>
2017-03-29 00:45:55 +0100

    Update Laravel socialite

--- config/app.php
+++ config/app.php
@@ -147,7 +147,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Laravel\Socialite\SocialiteServiceProvider::class,
 
         /*
          * Application Service Providers...
